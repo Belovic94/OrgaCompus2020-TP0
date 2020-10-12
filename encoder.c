e07b6c5d1651bec81f7fb5c8ca84812d396809e0
Belovic94 <victor.belosevich94@gmail.com>
2020-10-12 20:07:35 -0300

    Fix encoder

--- encoder.c
+++ encoder.c
@@ -1,6 +1,7 @@
+#include <stdio.h>
+#include "filereader.h"
 
-char *encode_base64(filereader_t file)
-{
+char *encode_base64(filereader_t* file) {
     //Leer hasta 3 bytes o lo que se pueda. Si no se completan, llenar con ceros.
     //Se encodea en base 64:
     //Pasar los 3 bytes a binario. Tomar 4 bloques de a 6 bits. Rellenar a la izquierda cada bloque
@@ -12,47 +13,26 @@ char *encode_base64(filereader_t file)
     char *base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
     char *encoded_data = 4 * ((input_length + 2) = 3);
-    char *bytes_in_binary;
-    int bytes_counter = 0;
+    size_t bytes_in_binary;
     char *bytes;
+    size_t index = 0;
+    int bytes_read = 0;
 
-    while (!filereader_eof(file))
-    {
-        filereader_read(file, bytes);
-        bytes_counter++;
-        if (bytes_counter == 3)
-        {
-            bytes_in_binary = bytes_to_binary(bytes);
-            encoded_data[0] = base64_chars[(bytes_in_binary >> 6 * 3) & 0x3F];
-            encoded_data[] = base64_chars[(>>) & 0x3F];
-            encoded_data[] = base64_chars[(>>) & 0x3F];
-            encoded_data[] = base64_chars[(>>) & 0x3F];
+    while (!filereader_eof(file)) {
+        bytes_read = filereader_read(file, bytes);
+        bytes_to_binary(bytes, &bytes_in_binary);
+        for (size_t i = 0; i < 4; i++) {
+            encoded_data[index++] = bytes_read >= i ? base64_chars[(bytes_in_binary >> 6 * (3 - i)) & 0x3F] : "=";        
         }
+        
     }
-    bytes_to_binary(bytes);
     return encoded_data;
 }
 
-char *bytes_to_binary(char *bytes)
-{
-    char output[8];
-    char bytes_in_binary[24];
-
-    for (int i = 0; i < 3; i++)
-    {
-        if (strcmp(bytes[i], ""))
-        {
-            bytes[i] = "=";
-        }
-        itoa(bytes[i], output, 2);
-        if (i == 0)
-        {
-            strncpy(bytes_in_binary, output, 8);
-        }
-        else
-        {
-            strncat(bytes_in_binary, output, 8);
-        }
+void bytes_to_binary(char *bytes, size_t *output) {
+    size_t aux = bytes[0];
+    for (int i = 1; i < 3; i++) {
+        aux = strcmp(bytes[i], "") == 0 ? aux << 8 | bytes[i] : aux << 8;
     }
-    return bytes_in_binary;
+    *output = aux;
 }
